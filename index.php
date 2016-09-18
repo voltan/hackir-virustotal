2982008d2bdb0e8f6e7961206b5323d8043cc65f
voltan <djvoltan@gmail.com>
2016-09-18 09:51:32 +0430

    Import template

---
+++ index.php
@@ -0,0 +1,173 @@
+<?php
+$pageInfo = array(
+    'title' => 'هک - امنیت اطلاعات - حریم خصوصی',
+    'keywords' => 'هک - امنیت اطلاعات - حریم خصوصی',
+    'description' => 'هک - امنیت اطلاعات - حریم خصوصی',
+    'author' => 'هک - امنیت اطلاعات - حریم خصوصی',
+    'generator' => 'هک - امنیت اطلاعات - حریم خصوصی',
+    'og' => array(
+        'title' => 'هک - امنیت اطلاعات - حریم خصوصی',
+        'site_name' => 'هک - امنیت اطلاعات - حریم خصوصی',
+        'description' => 'هک - امنیت اطلاعات - حریم خصوصی',
+        'url' => '',
+        'locale' => '',
+        'type' => '',
+        'image' => '',
+    ),
+    'twitter' => array(
+        'card' => '',
+        'site' => '',
+        'creator' => '',
+        'title' => 'هک - امنیت اطلاعات - حریم خصوصی',
+        'description' => 'هک - امنیت اطلاعات - حریم خصوصی',
+        'image' => '',
+        'url' => '',
+    ),
+    'brand_name' => 'هک',
+    'brand_url' => 'http://www.hack.ir',
+    'nav' => array(
+        '1' => array(
+            'title' => 'عنوان منو',
+            'url' => '#',
+        ),
+        '2' => array(
+            'title' => 'عنوان منو',
+            'url' => '#',
+        ),
+        '3' => array(
+            'title' => 'عنوان منو',
+            'url' => '#',
+        ),
+        '4' => array(
+            'title' => 'عنوان منو',
+            'url' => '#',
+        ),
+        '5' => array(
+            'title' => 'عنوان منو',
+            'url' => '#',
+        ),
+        '6' => array(
+            'title' => 'عنوان منو',
+            'url' => '#',
+        ),
+        '7' => array(
+            'title' => 'درباره ما',
+            'url' => '#',
+        ),
+        '8' => array(
+            'title' => 'تماس با ما',
+            'url' => '#',
+        ),
+    ),
+    'h1_title' => 'هک - امنیت اطلاعات - حریم خصوصی',
+);
+?>
+<!DOCTYPE HTML>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $pageInfo['title']; ?></title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+
+    <meta name="keywords" content="<?php echo $pageInfo['keywords']; ?>">
+    <meta name="description" content="<?php echo $pageInfo['description']; ?>">
+    <meta name="author" content="<?php echo $pageInfo['author']; ?>">
+    <meta name="generator" content="<?php echo $pageInfo['generator']; ?>">
+
+    <meta property="og:title" content="<?php echo $pageInfo['og']['title']; ?>">
+    <meta property="og:site_name" content="<?php echo $pageInfo['og']['site_name']; ?>">
+    <meta property="og:description" content="<?php echo $pageInfo['og']['description']; ?>">
+    <meta property="og:url" content="<?php echo $pageInfo['og']['url']; ?>">
+    <meta property="og:locale" content="<?php echo $pageInfo['og']['locale']; ?>">
+    <meta property="og:type" content="<?php echo $pageInfo['og']['type']; ?>">
+    <meta property="og:image" content="<?php echo $pageInfo['og']['image']; ?>">
+
+    <meta name="twitter:card" content="<?php echo $pageInfo['twitter']['card']; ?>">
+    <meta name="twitter:site" content="<?php echo $pageInfo['twitter']['site']; ?>">
+    <meta name="twitter:creator" content="<?php echo $pageInfo['twitter']['creator']; ?>">
+    <meta name="twitter:title" content="<?php echo $pageInfo['twitter']['title']; ?>">
+    <meta name="twitter:description" content="<?php echo $pageInfo['twitter']['description']; ?>">
+    <meta name="twitter:image" content="<?php echo $pageInfo['twitter']['image']; ?>">
+    <meta name="twitter:url" content="<?php echo $pageInfo['twitter']['url']; ?>">
+
+    <link href="./vendor/bootstrap/css/bootstrap.min.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="./vendor/font-awesome/css/font-awesome.min.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="./vendor/css/style.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="./vendor/css/locale.css" media="screen" rel="stylesheet" type="text/css">
+
+    <link href="./vendor/image/touch-icon-iphone.png" rel="apple-touch-icon">
+    <link href="./vendor/image/touch-icon-ipad.png" rel="apple-touch-icon">
+    <link href="./vendor/image/touch-icon-iphone-retina.png" rel="apple-touch-icon">
+    <link href="./vendor/image/touch-icon-ipad-retina.png" rel="apple-touch-icon">
+
+    <link href="./vendor/image/favicon.ico" rel="shortcut icon" type="image/x-icon">
+    <link href="./vendor/image/favicon.png" rel="shortcut icon" type="image/png">
+
+    <script type="text/javascript" src="./vendor/jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="./vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--[if lt IE 9]><script type="text/javascript" src="./vendor/js/html5shiv.min.js"></script><![endif]-->
+    <!--[if lt IE 9]><script type="text/javascript" src="./vendor/js/respond.min.js"></script><![endif]-->
+
+</head>
+<body id="template-body" class="template-body-content template-locale-fa">
+<nav class="template-navbar navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#template-navbar-main" aria-expanded="false">
+                <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand" href="<?php echo $pageInfo['brand_url']; ?>"><?php echo $pageInfo['brand_name']; ?></a>
+        </div>
+        <div class="collapse navbar-collapse" id="template-navbar-main">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="<?php echo $pageInfo['nav'][1]['url']; ?>"><?php echo $pageInfo['nav'][1]['title']; ?></a></li>
+                <li><a href="<?php echo $pageInfo['nav'][1]['url']; ?>"><?php echo $pageInfo['nav'][1]['title']; ?></a></li>
+                <li><a href="<?php echo $pageInfo['nav'][2]['url']; ?>"><?php echo $pageInfo['nav'][2]['title']; ?></a></li>
+                <li><a href="<?php echo $pageInfo['nav'][3]['url']; ?>"><?php echo $pageInfo['nav'][3]['title']; ?></a></li>
+                <li><a href="<?php echo $pageInfo['nav'][4]['url']; ?>"><?php echo $pageInfo['nav'][4]['title']; ?></a></li>
+                <li><a href="<?php echo $pageInfo['nav'][5]['url']; ?>"><?php echo $pageInfo['nav'][5]['title']; ?></a></li>
+                <li><a href="<?php echo $pageInfo['nav'][6]['url']; ?>"><?php echo $pageInfo['nav'][6]['title']; ?></a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right pull-right">
+                <li><a href="<?php echo $pageInfo['nav'][7]['url']; ?>"><?php echo $pageInfo['nav'][7]['title']; ?></a></li>
+                <li><a href="<?php echo $pageInfo['nav'][8]['url']; ?>"><?php echo $pageInfo['nav'][8]['title']; ?></a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<header class="template-header clearfix">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <ul class="list-inline">
+                    <li><img src="./vendor/image/logo.png" alt="<?php echo $pageInfo['h1_title']; ?>" /></li>
+                    <li><h1><?php echo $pageInfo['h1_title']; ?></h1></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
+<section class="template-section clearfix">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="template-section-content">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<footer class="template-footer clearfix">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p>استفاده از مطالب با ذکر منبع بلامانع است.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
