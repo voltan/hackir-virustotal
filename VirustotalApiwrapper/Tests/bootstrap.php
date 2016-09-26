90ed28bf13e4508935da083230b1b7ad1887ed82
voltan <djvoltan@gmail.com>
2016-09-26 10:12:16 +0330

    Import script

--- VirustotalApiwrapper/Tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Raise error reporting level
-// and displays all errors
-error_reporting( E_ALL | E_STRICT );
-ini_set('display_errors','On');
-ini_set('display_startup_errors','On');
-?>
--- index.php
+++ index.php
@@ -157,11 +157,11 @@ $pageInfo = array(
                     <?php if (isset($_FILES["file"]["name"]) && !empty($_FILES["file"]["name"]) && isset($_POST["submit"])) { ?>
                         <?php
                         // Use class
-                        require_once __DIR__ . '/../VirustotalApiwrapper/Vendors/autoload.php';
+                        //require_once __DIR__ . '/../VirustotalApiwrapper/Vendors/autoload.php';
                         // Set api key
-                        $apiKey = 'your_api_key';
+                        $apiKey = '19f2ab7ca76132a33559efbcca6c20d170520e4685ccd27e624a5a1f7dd596d2';
                         // Upload file
-                        $target_dir = "upload/";
+                        $target_dir = "/home/nanoin/public_html/antivirus/upload/";
                         $target_file = $target_dir . basename($_FILES["file"]["name"]);
                         $uploadOk = 1;
                         $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
@@ -176,7 +176,7 @@ $pageInfo = array(
                             $uploadOk = 0;
                         }
                         // Allow certain file formats
-                        if(!in_array($fileType, array('jpg', 'png', 'jpeg', 'gif'))) {
+                        if(!in_array($fileType, array('jpg', 'png', 'jpeg', 'gif', 'exe', 'pdf', 'doc', 'docx'))) {
                             echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
                             $uploadOk = 0;
                         }
@@ -187,13 +187,85 @@ $pageInfo = array(
                         } else {
                             if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
                                 try {
-                                    // Check file
-                                    $file = new VirusTotal\File($apiKey);
-                                    $resp = $file->scan($target_file);
+                                    /*
+                                    PHP code for scanning files for viruses using virustotal.com API
+                                    File coded by Adrian at www.TheWebHelp.com
+                                    */
+                                    header("Content-Type: text/plain");
+
+                                    // get the file size in mb, we will use it to know at what url to send for scanning (it's a different URL for over 30MB)
+                                    $file_size_mb = filesize($target_file)/1024/1024;
+
+                                    // calculate a hash of this file, we will use it as an unique ID when quering about this file
+                                    $file_hash = hash('sha256', file_get_contents($target_file));
+
+                                    // [PART 1] hecking if a report for this file already exists (check by providing the file hash (md5/sha1/sha256)
+                                    // or by providing a scan_id that you receive when posting a new file for scanning
+                                    // !!! NOTE that scan_id is the only one that indicates file is queued/pending, the others will only report once scan is completed !!!
+                                    $report_url = 'https://www.virustotal.com/vtapi/v2/file/report?apikey='.$apiKey."&resource=".$file_hash;
+
+                                    $api_reply = file_get_contents($report_url);
+
+                                    // convert the json reply to an array of variables
+                                    $api_reply_array = json_decode($api_reply, true);
+
+
+
+                                    // your resource is queued for analysis
+                                    if($api_reply_array['response_code']==-2){
+                                        echo $api_reply_array['verbose_msg'];
+                                    }
+
+                                    // reply is OK (it contains an antivirus report)
+                                    // use the variables from $api_reply_array to process the antivirus data
+                                    if($api_reply_array['response_code']==1){
+                                        echo "\nWe got an antivirus report, there were ".$api_reply_array['positives']." positives found. Here is the full data: \n\n";
+                                        print_r($api_reply_array);
+                                        exit;
+                                    }
+
+                                    // [PART 2] a report for this file was not found, upload file for scanning
+                                    if($api_reply_array['response_code']=='0'){
+
+                                        // default url where to post files
+                                        $post_url = 'https://www.virustotal.com/vtapi/v2/file/scan';
+
+                                        // get a special URL for uploading files larger than 32MB (up to 200MB)
+                                        if($file_size_mb >= 32){
+                                            $api_reply = @file_get_contents('https://www.virustotal.com/vtapi/v2/file/scan/upload_url?apikey='.$apiKey);
+                                            $api_reply_array = json_decode($api_reply, true);
+                                            if(isset($api_reply_array['upload_url']) and $api_reply_array['upload_url']!=''){
+                                                $post_url = $api_reply_array['upload_url'];
+                                            }
+                                        }
+
+                                        // send a file for checking
+
+                                        // curl will accept an array here too:
+                                        $post['apikey'] = $apiKey;
+                                        $post['file'] = '@'.$target_file;
+
+                                        $ch = curl_init();
+                                        curl_setopt($ch, CURLOPT_URL,$post_url);
+                                        curl_setopt($ch, CURLOPT_POST,1);
+                                        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                                        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+                                        $api_reply = curl_exec ($ch);
+                                        curl_close ($ch);
+
+                                        $api_reply_array = json_decode($api_reply, true);
+
+                                        if($api_reply_array['response_code']==1){
+                                            echo "\nfile queued OK, you can use this scan_id to check the scan progress:\n".$api_reply_array['scan_id'];
+                                            echo "\nor just keep checking using the file hash, but it will only report once it is completed (no 'PENDING/QUEUED' reply will be given).";
+                                        }
+
+                                    }
+
                                     // Show result
-                                    echo '<pre>';
-                                    print_r($resp);
-                                    echo '</pre>';
+                                    //echo '<pre>';
+                                    //print_r($resp);
+                                    //echo '</pre>';
                                 } catch (Exception $e) {
                                     // Show error
                                     echo '<pre>';
